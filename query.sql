e56ba7f3df75dbbff9649a60d3bce22f4ebe3ae2
Olga Rogova <Olga.Rogova_EX@kdrp.com>
2021-09-13 13:15:17 +0400

    UTF-8

--- query.sql
+++ query.sql
@@ -1,25 +1,25 @@
--- Поиск products по title
+-- РџРѕРёСЃРє products РїРѕ title
 select * from products where title = 'ACADEMY ATLANTIS' ;
 
--- Поиск products по price = 9.99 и category = 8 и отсортировать по category и price
+-- РџРѕРёСЃРє products РїРѕ price = 9.99 Рё category = 8В Рё РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°С‚СЊ РїРѕ category Рё price
 select * from products where price = 9.99 and category = 8 order by category, price;
 
--- Поиск products у которых category = 8 или 15
+-- РџРѕРёСЃРє products Сѓ РєРѕС‚РѕСЂС‹С… category = 8 РёР»Рё 15
 select * from products where category = 8 or category = 15;
 
--- Поиск products у которых price между 10 и 20 (используйте BETWEEN)
+-- РџРѕРёСЃРє products Сѓ РєРѕС‚РѕСЂС‹С… price РјРµР¶РґСѓ 10 Рё 20 (РёСЃРїРѕР»СЊР·СѓР№С‚Рµ BETWEEN)
 select * from products where price between 10 and 20;
 
--- Поиск orders у которых orderdate между 2004-01-05 и 2004-02-05
+-- РџРѕРёСЃРє orders Сѓ РєРѕС‚РѕСЂС‹С… orderdate РјРµР¶РґСѓ 2004-01-05 Рё 2004-02-05
 select * from orders where orderdate between '2004-01-05' and '2004-02-05';
 
--- Сгруппировать данные в orders по полю customerid и посчитать количество относительно каждого customerid
+-- РЎРіСЂСѓРїРїРёСЂРѕРІР°С‚СЊ РґР°РЅРЅС‹Рµ РІ orders РїРѕ РїРѕР»СЋ customerid Рё РїРѕСЃС‡РёС‚Р°С‚СЊ РєРѕР»РёС‡РµСЃС‚РІРѕ РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ РєР°Р¶РґРѕРіРѕ customerid
 select customerid, count(customerid) from orders group by customerid; 
 
--- Сгруппировать данные в orders по полям customerid и orderdate и просуммировать totalamount, при этом сумма totalamount должна быть больше 100
+-- РЎРіСЂСѓРїРїРёСЂРѕРІР°С‚СЊ РґР°РЅРЅС‹Рµ РІ orders РїРѕ РїРѕР»СЏРј customerid Рё orderdate Рё РїСЂРѕСЃСѓРјРјРёСЂРѕРІР°С‚СЊ totalamount, РїСЂРё СЌС‚РѕРј СЃСѓРјРјР° totalamount РґРѕР»Р¶РЅР° Р±С‹С‚СЊ Р±РѕР»СЊС€Рµ 100
 select customerid, orderdate, sum(totalamount) from orders group by customerid, orderdate having sum(totalamount) > 100;
 
--- Написать запрос к таблицам customer, orders, orderlines и products с использованием JOIN и вывести firstname, lastname, title, orderdate
+-- РќР°РїРёСЃР°С‚СЊ Р·Р°РїСЂРѕСЃ Рє С‚Р°Р±Р»РёС†Р°Рј customer, orders, orderlines Рё products СЃ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµРј JOIN Рё РІС‹РІРµСЃС‚Рё firstname, lastname, title, orderdate
 select c.firstname, c.lastname, p.title, o.orderdate 
 from customers c
 inner join orders o on c.customerid = o.customerid
